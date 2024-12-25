9af2122e72c9d8d6f57df97fe952a2a1317dbf77
Chesapeake <git@moth.monster>
2024-12-25 00:54:21 -0500

    Stop putting grenades in cans

--- control.lua
+++ control.lua
@@ -1,6 +1,15 @@
+local function contains(list, x)
+	for _, v in ipairs(list) do
+		if v == x then return true end
+	end
+	return false
+end
+
+local canTypes = {"canned-fish", "canned-salmon", "canned-cod", "canned-pufferfish", "canned-clownfish"}
+
 script.on_event(defines.events.on_player_used_capsule,
 function(event)
-	if (event.item.name == "canned-fish" or "canned-salmon" or "canned-cod" or "canned-pufferfish" or "canned-clownfish") then
+	if contains(canTypes, event.item.name) then
 		player = game.get_player(event.player_index)
 		player.get_main_inventory().insert({name="used-can", count=1})
 	end
